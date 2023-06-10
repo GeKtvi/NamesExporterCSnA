819f46ed1f6685a8f0e590e07f6b0cf4ddbf428c
GeKtvi <61162497+GeKtvi@users.noreply.github.com>
2023-06-10 10:49:34 +0300

    Reworked app startup

--- NamesExporterCSnA/App.xaml.cs
+++ NamesExporterCSnA/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NamesExporterCSnA.Model;
+using NamesExporterCSnA.View;
+using NamesExporterCSnA.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +16,32 @@ namespace NamesExporterCSnA
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            try
+            {
+                MainWindowModel model = null;
+                MainWindowViewModel viewModel = null;
+                MainWindowView view = null;
+
+                model = new MainWindowModel();
+                viewModel = new MainWindowViewModel(model);
+                view = new MainWindowView(viewModel);
+
+                view.Show();
+            }
+            catch (Exception)
+            {
+                //view?.Close();
+                //app?.Shutdown();
+
+#if !DEBUG
+                    MessageBox.Show(new Window(),"Не обрабатываемая ошибка выполнения программы", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+#else
+                throw;
+#endif
+            }
+        }
     }
 }
