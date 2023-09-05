5d6c88d378b029e0b118ddf8217bcba9c8b5da17
GeKtvi <61162497+GeKtvi@users.noreply.github.com>
2023-09-05 21:39:06 +0300

    Cleanup

--- NamesExporterCSnA/Model/MainWindowModel.cs
+++ NamesExporterCSnA/Model/MainWindowModel.cs
@@ -1,5 +1,5 @@
-﻿using DynamicData;
-using DynamicData.Binding;
+﻿using DynamicData.Binding;
+using GeKtviWpfToolkit;
 using NamesExporterCSnA.Data;
 using NamesExporterCSnA.Data.UpdateLog;
 using Prism.Mvvm;
@@ -18,31 +18,24 @@ namespace NamesExporterCSnA.Model
 {
     public class MainWindowModel : BindableBase
     {
-        public ObservableCollectionExtended<MaxExportedCable> DataIn => _dataIn;
+        public ObservableCollectionExtended<MaxExportedCable> DataIn { get; }
         public ReadOnlyObservableCollection<IDisplayableData> DataOut => new ReadOnlyObservableCollection<IDisplayableData>(_dataOut);
         public IUpdateLogger Logger => _converter.Logger;
         public bool IsUpdateFrozen { get; set; } = false;
 
-        private readonly ObservableCollectionExtended<MaxExportedCable> _dataIn;
         private readonly ObservableCollectionExtended<IDisplayableData> _dataOut;
         private readonly Dispatcher _dispatcher;
         private DataConverter _converter;
-        private Task<List<IDisplayableData>> _convertTask;
 
         public MainWindowModel(DataConverter converter)
         {
-            _dataIn = new ObservableCollectionExtended<MaxExportedCable>();
-
+            DataIn = new ObservableCollectionExtended<MaxExportedCable>();
             _dataOut = new ObservableCollectionExtended<IDisplayableData>();
-            //DynamicData.Binding.ObservableCollectionAdaptor
-            //DataOut = new ReadOnlyObservableCollection<IDisplayableData>(_dataOut);
-            //_dataOut.Connect().To
+
             _converter = converter;
             converter.SettingsChanged += () => UpdateDataOut();
 
             _dispatcher = Dispatcher.CurrentDispatcher;
-            //Dispatcher disp = Dispatcher.CurrentDispatcher;
-            //_deferredUpdate = new DeferredOperation(() => disp.BeginInvoke(UpdateDataOut), 5);
         }
 
         public void SetDataIn(List<string[]> values)
@@ -52,10 +45,9 @@ namespace NamesExporterCSnA.Model
             Stopwatch swSetDataIn = Stopwatch.StartNew();
 #endif
             #endregion
-            //IsUpdateFrozen = true;
-            var suspend = _dataIn.SuspendNotifications();
-
-            _dataIn.Clear();
+            IDisposable suspend = DataIn.SuspendNotifications();
+            
+            DataIn.Clear();
 
             foreach (string[] row in values)
             {
@@ -76,16 +68,21 @@ namespace NamesExporterCSnA.Model
                     }
                     i++;
                 }
-                _dataIn.Add(cable);
+                DataIn.Add(cable);
             }
 
             suspend.Dispose();
-            //IsUpdateFrozen = false;
             #region Debug
 #if DEBUG
             Debug.WriteLine($"SetDataIn time - {swSetDataIn.ElapsedMilliseconds} ms");
 #endif
             #endregion
+        }
+
+        public void SetTextFromClipboard()
+        {
+            List<string[]> data = ClipboardHelper.ParseClipboardData();
+            SetDataIn(data ?? new List<string[]>());
         }
 
         public List<List<string>> GetDataAsListList()
@@ -110,43 +107,18 @@ namespace NamesExporterCSnA.Model
             return data;
         }
 
-        public async void UpdateDataOut()
+        public  void UpdateDataOut()
         {
             #region Debug
 #if DEBUG
             Debug.WriteLine($"Start UpdateDataOut - {DateTime.Now}");
-#endif
-            #endregion
-
-            if (IsUpdateFrozen)
-                return;
-
-            #region Debug
-#if DEBUG
             Stopwatch sw = Stopwatch.StartNew();
 #endif
             #endregion
+            //Thread.Sleep(5000);
+            throw new InvalidOperationException("");
+            List<IDisplayableData> data =  Task.Run(() => _converter.Convert(DataIn.ToList())).Result;
 
-            Task<List<IDisplayableData>> currentTask = new Task<List<IDisplayableData>>(() => _converter.Convert(_dataIn.ToList()));
-            _convertTask = currentTask;
-            currentTask.Start();
-
-            //            #region Debug
-            //#if DEBUG
-            //            Stopwatch swClear = Stopwatch.StartNew();
-            //#endif
-            //            #endregion
-
-            //            #region Debug
-            //#if DEBUG
-            //            Debug.WriteLine($"Clear time - {swClear.ElapsedMilliseconds} ms");
-            //#endif
-            //            #endregion
-            //var lst = _dataIn.ToList();
-            List<IDisplayableData> data = await currentTask;//Task.Run(() => _converter.Convert(lst)); //new Task<List<IDisplayableData>>(() => _converter.Convert(_dataIn.ToList())).Start();
-
-            //if (_convertTask != currentTask)
-            //    return;
             _dispatcher.Invoke(() =>
             {
                 using (_dataOut.SuspendNotifications())
--- NamesExporterCSnA/ViewModel/MainWindowViewModel.cs
+++ NamesExporterCSnA/ViewModel/MainWindowViewModel.cs
@@ -7,15 +7,12 @@ using NamesExporterCSnA.Model;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Reactive;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Windows.Input;
 
 namespace NamesExporterCSnA.ViewModel
 {
@@ -40,61 +37,34 @@ namespace NamesExporterCSnA.ViewModel
         public MainWindowViewModel(MainWindowModel mainWindowModel)
         {
             _mainWindowModel = mainWindowModel;
-
-            UpdateDataOut = ReactiveCommand.CreateFromObservable(() => Observable.Start(() => _mainWindowModel.UpdateDataOut()));
-            UpdateDataOut.ThrownExceptions.Subscribe(e => throw e.InnerException);
+            
+            UpdateDataOut = ReactiveCommand.CreateRunInBackground(() => _mainWindowModel.UpdateDataOut());
+            UpdateDataOut.ThrownExceptions.Subscribe(e => throw e);
 
             ImportData = ReactiveCommand.Create(
-                SetTextFromClipboard,
+                _mainWindowModel.SetTextFromClipboard,
                 UpdateDataOut.IsExecuting.Select(x => x == false),
                 RxApp.MainThreadScheduler);
+            ImportData.ThrownExceptions.Subscribe(e => throw e);
 
             ExportData = ReactiveCommand.Create(
-                SetTextToClipboard,
-                _mainWindowModel.DataOut.ToObservableChangeSet().Throttle(TimeSpan.FromMilliseconds(25), DispatcherScheduler.Current).Select(data => data != null && data.Count != 0)
+                () => ClipboardHelper.SetClipboardData(_mainWindowModel.GetDataAsListList()),
+                _mainWindowModel.DataOut.ToObservableChangeSet().Select(data => data != null && data.Count != 0)
             );
+            ExportData.ThrownExceptions.Subscribe(e => throw e);
 
             ClearData = ReactiveCommand.Create(
-                ClearDataIn,
+                _mainWindowModel.DataIn.Clear,
                 _mainWindowModel.DataIn.ToObservableChangeSet().Select(data => data != null && data.Count != 0)
             );
-
-            //_updateDataOut = ReactiveCommand.Create(() => _mainWindowModel.UpdateDataOut(), outputScheduler: DispatcherScheduler.Current);
-
+            ClearData.ThrownExceptions.Subscribe(e => throw e);
 
             _mainWindowModel.DataIn.ToObservableChangeSet()
                 .ObserveOn(RxApp.TaskpoolScheduler)
                 .Throttle(TimeSpan.FromMilliseconds(25))
                 .AutoRefresh()
-                //.Select(number => Observable.Defer(() => _mainWindowModel.UpdateDataOut().ToObservable()))
-                //.Select(number => Observable.FromAsync(async () => await _mainWindowModel.UpdateDataOut()))
-                //.Select(number => Observable.FromAsync(async () => await disp.BeginInvoke(() => _mainWindowModel.UpdateDataOut())))
-                //.Select(number => Observable.Defer(() => Observable.Start(() => disp.BeginInvoke(() => _mainWindowModel.UpdateDataOut()))))
-                //.Select(l => Observable.FromAsync(_ => _mainWindowModel.UpdateDataOut()))
-                //.Concat()
-                //.Select(x => Unit.Default)
-                //.Select(x => Unit.Default)
                 .Select(x => Unit.Default)
-                .InvokeCommand(UpdateDataOut)
-                //.Subscribe()
-                ;
-        }
-
-        private void SetTextFromClipboard()
-        {
-            List<string[]> data = ClipboardHelper.ParseClipboardData();
-            if (data != null)
-                _mainWindowModel.SetDataIn(data);
-        }
-
-        private void SetTextToClipboard()
-        {
-            ClipboardHelper.SetClipboardData(_mainWindowModel.GetDataAsListList());
-        }
-
-        private void ClearDataIn()
-        {
-            _mainWindowModel.DataIn.Clear();
+                .InvokeCommand(UpdateDataOut);
         }
     }
 }
